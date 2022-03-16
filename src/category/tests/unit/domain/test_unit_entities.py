5047ef2c992e4938af1d5268a1d1750d70e983f6
candidosouza <candidosouzza@gmail.com>
2022-03-15 21:47:50 -0300

    :white_check_mark: Tests entity category dataclass and properties

--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -2,13 +2,30 @@ import unittest
 
 import unittest
 from datetime import datetime
+from dataclasses import is_dataclass
 from category.domain.entities import Category
 
-class TestCategory(unittest.TestCase):
+class TestCategoryUnit(unittest.TestCase):
+
+    def test_if_is_a_dataclass(self):
+        self.assertTrue(is_dataclass(Category))
 
     def test_constructor(self):
-        category = Category('Movie', 'Some description', True, datetime.now())
+        category = Category(name='Movie')
         self.assertEqual(category.name, 'Movie')
-        self.assertEqual(category.description, 'Some description')
+        self.assertEqual(category.description, None)
         self.assertEqual(category.is_active, True)
         self.assertIsInstance(category.created_at, datetime)
+
+        created_at = datetime.now()
+        category = Category(
+            name='Movie', 
+            description='some description', 
+            is_active=False, 
+            created_at=created_at
+        )
+
+        self.assertEqual(category.name, 'Movie')
+        self.assertEqual(category.description, 'some description')
+        self.assertEqual(category.is_active, False)
+        self.assertEqual(category.created_at, created_at)
