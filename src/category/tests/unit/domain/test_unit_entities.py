515b8d8cc6ef2400aa18a5c4d81bd52a6c94c618
candidosouza <candidosouzza@gmail.com>
2022-03-15 21:59:03 -0300

    :white_check_mark: Test entity category created_at

--- src/category/tests/unit/domain/test_unit_entities.py
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -19,13 +19,25 @@ class TestCategoryUnit(unittest.TestCase):
 
         created_at = datetime.now()
         category = Category(
-            name='Movie', 
-            description='some description', 
-            is_active=False, 
-            created_at=created_at
+            name        = 'Movie',
+            description = 'some description',
+            is_active   = False,
+            created_at  = created_at
         )
 
         self.assertEqual(category.name, 'Movie')
         self.assertEqual(category.description, 'some description')
         self.assertEqual(category.is_active, False)
         self.assertEqual(category.created_at, created_at)
+
+    def test_if_created_at_is_generated_in_constructor(self):
+        category1 = Category(name='Movie 1')
+        category2 = Category(name='Movie 2')
+        # self.assertNotEqual(
+        #     category1.created_at,
+        #     category2.created_at
+        # )
+        self.assertNotEqual(
+            category1.created_at.timestamp(),
+            category2.created_at.timestamp()
+        )
