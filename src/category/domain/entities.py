7d96bc8ea52ade438d3e18d677d6fd06149691d5
candidosouza <candidosouzza@gmail.com>
2022-03-15 21:09:28 -0300

    :label: Changing category entity and using dataclass

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,11 +1,11 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
 
+@dataclass
 class Category:
 
-    __slots__ = ['__name']
-
-    def __init__(self, name) -> None:
-        self.__name = name
-
-    def __str__(self) -> str:
-        return f'Category {self.__name}'
- 
+    name       : str
+    description: Optional[str] = None
+    is_active  : Optional[bool] = True
+    created_at : Optional[datetime] = field(default_factory=lambda: datetime.now())
