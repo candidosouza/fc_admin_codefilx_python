9d3e44b74c26c5684625c874f216709a7d7ddd72
candidosouza <candidosouzza@gmail.com>
2022-03-30 09:25:56 -0300

    :sparkles: Abstraction for valeu object

--- src/__seedwork/domain/value_objects.py
+++ src/__seedwork/domain/value_objects.py
@@ -1,9 +1,22 @@
 import uuid
-from dataclasses import dataclass, field
+import json
+from abc import ABC
+from dataclasses import dataclass, field, fields
 from __seedwork.domain.exceptions import InvalidUuidException
 
+
 @dataclass(frozen=True)
-class UniqueEntityId:
+class ValueObject(ABC):
+
+    def __str__(self):
+        fields_name = [field.name for field in fields(self)]
+        return str(getattr(self, fields_name[0])) \
+            if len(fields_name) == 1 \
+            else json.dumps({field_name: getattr(self, field_name) for field_name in fields_name})
+
+
+@dataclass(frozen=True)
+class UniqueEntityId(ValueObject):
     id: str = field(
         default_factory=lambda: str(uuid.uuid4())
     )
