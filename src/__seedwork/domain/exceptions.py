b48d7fdd92b009f35856d22f72aa0ca7900b50ff
candidosouza <candidosouzza@gmail.com>
2023-04-04 17:21:36 -0300

    feat: add entity validation exception

--- src/__seedwork/domain/exceptions.py
+++ src/__seedwork/domain/exceptions.py
@@ -1,4 +1,7 @@
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from __seedwork.domain.validators import ErrorFields
 
 class InvalidUuidException(Exception):
     def __init__(self, error='ID must be a valid UUID') -> None:
@@ -7,3 +10,11 @@ class InvalidUuidException(Exception):
 
 class ValidationException(Exception):
     pass
+
+
+class EntityValidationException(Exception):
+    error: 'ErrorFields'
+
+    def __init__(self, error: 'ErrorFields') -> None:
+        self.error = error
+        super().__init__('Entity validation error')
