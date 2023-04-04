28350e65275c25ee34c740e7e97db1bf19773ddb
candidosouza <candidosouzza@gmail.com>
2023-04-04 08:55:06 -0300

    fix: fix category domain validator

--- src/category/domain/validators.py
+++ src/category/domain/validators.py
@@ -1,24 +1,24 @@
-from typing import Any
-
+from typing import Dict
 from rest_framework import serializers
+from __seedwork.domain.validators import DRFValidator, StrictBooleanField, StrictCharField
 
-from __seedwork.domain.validators import DRFValidator, StrictCharField, BooleanField
+# pylint: disable=abstract-method
 
 
 class CategoryRules(serializers.Serializer):
     name = StrictCharField(max_length=255, min_length=3)
     description = StrictCharField(required=False, allow_null=True, allow_blank=True)
-    is_active = BooleanField(required=False)
+    is_active = StrictBooleanField(required=False)
     created_at = serializers.DateTimeField(required=False)
 
 
-class Categoryvalidator(DRFValidator):
-    def validate(self, data: Any) -> bool:
-        rules = CategoryRules(data=data)
+class CategoryValidator(DRFValidator):
+    def validate(self, data: Dict) -> bool:
+        rules = CategoryRules(data=data if data is not None else {})
         return super().validate(rules)
 
 
-class CategoryValidatorFactory:
+class CategoryValidatorFactory:  # pylint: disable=too-few-public-methods
     @staticmethod
     def create():
-        return Categoryvalidator()
+        return CategoryValidator()
