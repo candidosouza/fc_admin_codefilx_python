6d4178e5da9f3db46c98187702fd9c8e32a482f3
candidosouza <candidosouzza@gmail.com>
2023-04-18 21:47:04 -0300

    chore: add folder code on src

---
+++ src/core/__seedwork/tests/unit/test_unit_repositories.py
@@ -0,0 +1,17 @@
+import unittest
+
+from core.__seedwork.domain.repositories import (
+    RepositoryInterface,
+)
+
+
+class TestRepositoryInterface(unittest.TestCase):
+
+    def test_throw_error_when_methods_not_implemented(self):
+        with self.assertRaises(TypeError) as assert_error:
+            # pylint: disable=abstract-class-instantiated
+            RepositoryInterface()
+        self.assertEqual(assert_error.exception.args[0],
+                        "Can't instantiate abstract class RepositoryInterface with abstract " +
+                        "methods delete, find_all, find_by_id, insert, update"
+                        )
