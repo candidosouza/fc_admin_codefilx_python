721f010e5f54d9ca9b7934764e4a77cc893e8a8a
candidosouza <candidosouzza@gmail.com>
2023-04-24 19:55:41 -0300

    feat: add custom category

--- src/core/category/domain/repositories.py
+++ src/core/category/domain/repositories.py
@@ -1,6 +1,20 @@
 from abc import ABC
-from core.__seedwork.domain.repositories import RepositoryInterface
+from core.__seedwork.domain.repositories import (
+    SearchParams as DesfaultSearchParams,
+    SearchResult as DefaultSearchResult,
+    SearchableRepositoryInterface
+)
 from core.category.domain.entities import Category
 
-class CategoryRepository(RepositoryInterface[Category], ABC):
+
+class _SearchParams(DesfaultSearchParams):
     pass
+
+
+class _SearchResult(DefaultSearchResult):
+    pass
+
+
+class CategoryRepository(SearchableRepositoryInterface[Category, _SearchParams, _SearchResult], ABC):
+    SearchParams = _SearchParams
+    SearchResult = _SearchResult
