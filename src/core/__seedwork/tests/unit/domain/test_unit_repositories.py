71902debfb7cab597e72b057cef6212f2961a2e2
candidosouza <candidosouzza@gmail.com>
2023-04-24 15:50:36 -0300

    test: unit tests result search

--- src/core/__seedwork/tests/unit/domain/test_unit_repositories.py
+++ src/core/__seedwork/tests/unit/domain/test_unit_repositories.py
@@ -276,3 +276,76 @@ class TestSearchParams(unittest.TestCase):
             self.assertEqual(params.filter, i['expected'], i)
 
 
+class TestSearchResult(unittest.TestCase):
+
+    def test_props_annotations(self):
+        self.assertEqual(SearchResult.__annotations__, {
+            'items': List[ET],
+            'total': int,
+            'current_page': int,
+            'per_page': int,
+            'last_page': int,
+            'sort': Optional[str],
+            'sort_dir': Optional[str],
+            'filter': Optional[Filter]
+        })
+
+    def test_constructor(self):
+        entity = StubEntity(name='fake', price=5)
+        result = SearchResult(
+            items=[entity, entity],
+            total=4,
+            current_page=1,
+            per_page=2
+        )
+
+        self.assertDictEqual(result.to_dict(), {
+            'items': [entity, entity],
+            'total': 4,
+            'current_page': 1,
+            'per_page': 2,
+            'last_page': 2,
+            'sort': None,
+            'sort_dir': None,
+            'filter': None
+        })
+
+        result = SearchResult(
+            items=[entity, entity],
+            total=4,
+            current_page=1,
+            per_page=2,
+            sort='name',
+            sort_dir='asc',
+            filter='test'
+        )
+
+        self.assertDictEqual(result.to_dict(), {
+            'items': [entity, entity],
+            'total': 4,
+            'current_page': 1,
+            'per_page': 2,
+            'last_page': 2,
+            'sort': 'name',
+            'sort_dir': 'asc',
+            'filter': 'test'
+        })
+
+    def test_when_per_page_is_greater_than_total(self):
+        result = SearchResult(
+            items=[],
+            total=4,
+            current_page=1,
+            per_page=15,
+        )
+        self.assertEqual(result.last_page, 1)
+
+    def test_when_per_page_is_less_than_total_and_they_are_not_multiples(self):
+        result = SearchResult(
+            items=[],
+            total=101,
+            current_page=1,
+            per_page=20,
+        )
+        self.assertEqual(result.last_page, 6)
+
