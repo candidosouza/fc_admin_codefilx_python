9b464115af6c3389089337e8841bdbe6cb8aadf9
candidosouza <candidosouzza@gmail.com>
2023-04-27 16:42:27 -0300

    refactor: change views with container di

---
+++ src/core/category/infra/django/api.py
@@ -0,0 +1,28 @@
+from dataclasses import asdict, dataclass
+from typing import Callable
+
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from core.category.application.use_cases import (
+    CreateCategoryUseCase,
+    ListCategoriesUseCase
+)
+
+
+@dataclass(slots=True)
+class CategoryResource(APIView):
+
+    create_use_case: Callable[[], CreateCategoryUseCase]
+    list_use_case: Callable[[], ListCategoriesUseCase]
+
+    def post(self, request: Request):
+        input_param = CreateCategoryUseCase.Input(request.data['name'])
+        output = self.create_use_case().execute(input_param)
+        return Response(asdict(output))
+
+    def get(self, request: Request):
+        input_param = ListCategoriesUseCase.Input(**request.query_params.dict())
+        output = self.list_use_case().execute(input_param)
+        return Response(asdict(output))
