3b8fe5c7b17348d07323875bf728a8bed8ca33da
candidosouza <candidosouzza@gmail.com>
2023-05-03 08:53:20 -0300

    feat: add category model

--- src/core/category/infra/django_app/models.py
+++ src/core/category/infra/django_app/models.py
@@ -1,3 +1,16 @@
-from django.db import models  # pylint: disable=unused-import
+from django.db import models
 
-# Create your models here.
+class CategoryModel(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return f'{self.name}'
