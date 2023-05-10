81268dd599864de2d24d94f1ac211fe2e820aed8
candidosouza <candidosouzza@gmail.com>
2023-05-10 20:43:29 -0300

    ci: remove stpe tests

--- src/django_app/settings.py
+++ src/django_app/settings.py
@@ -95,20 +95,11 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # prometheus monitoring your databases
-#         'ENGINE': 'django_prometheus.db.backends.sqlite3',
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         # prometheus monitoring your databases
-        # 'ENGINE': 'django_prometheus.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
